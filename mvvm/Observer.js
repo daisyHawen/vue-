226441be059f237541d6fee1dc505238bf5be1bd
daisyHawen <512309453@qq.com>
2017-03-15 15:25:23 +0800

    compile && wacher

--- mvvm/Observer.js
+++ mvvm/Observer.js
@@ -1,43 +1,62 @@
-var data = {
-	name: 'daisyHawen'
-};
-observe(data);
-data.name = 'dmq';
+ //那么将需要observe的数据对象进行递归遍历，
+ //包括子属性对象的属性，都加上 setter和getter
+ //这样的话，给这个对象的某个值赋值，
+ //就会触发setter，那么就能监听到了数据变化
 
-function observe(data) {
-	if (!data || typeof data !== 'object') {
-		return;
-	}
-	//取出所有属性遍历
-	Object.keys(data).forEach(function(key) {
-		defineReactive(data, key, data[key])
-	});
-};
-//利用Object.defineProperty监听属性变动
-function defineReactive(data, key, val) {
-	observe(val);
-	Object.defineProperty(data, key, {
-		enumerable: true, //可枚举
-		configurable: false, //不能再define
-		get: function() {
-			return val
-		},
-		set: function(newVal) {
-			console.log("哈哈，监听到值变化了", val, '--->', newVal);
-			val = newVal;
-		}
-	})
-}
+ var data = {
+ 	name: 'daisyHawen'
+ };
+ observe(data);
+ data.name = 'dmq';
 
-/*Writable 属性
-当属性特性（property attribute） writable 
-设置为false时，表示 non-writable，属性不能被修改。
-------------------------
-Enumerable 特性
-属性特性 enumerable 定义了对象的属性是否可以在
- for...in 循环和 Object.keys() 中被枚举。
---------------------------
-Configurable 特性
-configurable 特性表示对象的属性是否可以被删除，
-以及除 writable 特性外的其他特性是否可以被修改。
-*/
+ function observe(data) {
+ 	if (!data || typeof data !== 'object') {
+ 		return;
+ 	}
+ 	//取出所有属性遍历
+ 	Object.keys(data).forEach(function(key) {
+ 		defineReactive(data, key, data[key])
+ 	});
+ };
+ //利用Object.defineProperty监听属性变动
+ function defineReactive(data, key, val) {
+ 	observe(val); //监听子属性
+
+ 	Object.defineProperty(data, key, {
+ 		enumerable: true, //可枚举
+ 		configurable: false, //不能再define
+ 		get: function() {
+ 			// 由于需要在闭包内添加watcher，所以通过Dep定义一个全局target属性，暂存watcher, 添加完移除
+ 			Dep.target && dep.addDep(Dep.target)
+ 			return val
+ 		},
+ 		set: function(newVal) {
+ 			if (val === newVal) return; // 没有变化
+ 			console.log("哈哈，监听到值变化了", val, '--->', newVal);
+ 			val = newVal;
+ 			dep.notify(); //通知所有订阅者
+ 		}
+ 	})
+ }
+
+ function Dep() {
+ 	this.subs = [];
+ }
+ Dep.prototype = {
+ 	addSub: function(sub) {
+ 		this.subs.push(sub) //添加订阅者
+ 	},
+ 	notify: function() {
+ 		//通知所有的订阅者消息
+ 		this.subs.forEach(function(sub) {
+ 			sub.update();
+ 		})
+ 	}
+ }
+ Watcher.prototype = {
+ 	get: function(key) {
+ 		Dep.target = this;
+ 		this.value = data[key];
+ 		Dep.target = null;
+ 	}
+ }
--- mvvm/defineProperty.js
+++ mvvm/defineProperty.js
@@ -52,3 +52,15 @@ Object.defineProperty(o, "b", {
 o.b = 38; //set
 console.log(o.b); //get
 console.log(o); //{ a: 37, b: [Getter/Setter] }
+/*Writable 属性
+当属性特性（property attribute） writable 
+设置为false时，表示 non-writable，属性不能被修改。
+------------------------
+Enumerable 特性
+属性特性 enumerable 定义了对象的属性是否可以在
+ for...in 循环和 Object.keys() 中被枚举。
+--------------------------
+Configurable 特性
+configurable 特性表示对象的属性是否可以被删除，
+以及除 writable 特性外的其他特性是否可以被修改。
+*/
